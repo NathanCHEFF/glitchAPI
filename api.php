90677c48b88382a3a3b046b3365963a9e3536eea
Vasiliy Pupkin <vpupkin@company.com>
2020-04-05 17:08:55 -0700

    update src api.php

--- api.php
+++ api.php
@@ -1,18 +1,16 @@
 <?php
 #comment with debuging
-//error_reporting(0);			//for clothe 
-//display_errors(false); 		//
+//error_reporting(E_ALL);			//for clothe
+//display_errors(1); 		//
 #
 class api
 {
-	//const API_ENDPOINT 	 = '';			//ENDPOINT   kill him
+
 	const API_VER_PRFX   = 'v';
 	const API_MTD_PRFX   = 'method';
 	const API_VER_LIST   = 'versionlist.json';
-	
-	
+
 	private static $instance;
-	
 
 	public static function getApi(){
 		//GET or POST
@@ -45,30 +43,33 @@ class api
 	}
 
 	function load(){
-		//echo $this->getEndPoint(). $this->getVerList()->$_GET[SELF::API_VER_PRFX].$_GET[SELF::API_MTD_PRFX].'.php';
-		
-		if( file_exists( $this->getVerList()->$_GET[SELF::API_VER_PRFX]. $_GET[SELF::API_MTD_PRFX].'.php')){
-			include_once( $this->getVerList()->$_GET[SELF::API_VER_PRFX].$_GET[SELF::API_MTD_PRFX].'.php');   // ЦЫМЕС
+		//echo $this->getEndPoint(). $this->getVerList()->$_GET[SELF::API_VER_PRFX].$_GET[SELF::API_MTD_PRFX].'.php';\
+		$API_VER_PRFX = $_GET[SELF::API_VER_PRFX];
+		//var_dump( $this->getVerList()->$API_VER_PRFX . $_GET[SELF::API_MTD_PRFX].'.php');
+		//echo "\n";
+		if( file_exists( $this->getVerList()->$API_VER_PRFX . $_GET[SELF::API_MTD_PRFX].'.php')){
+			include_once( $this->getVerList()->$API_VER_PRFX .$_GET[SELF::API_MTD_PRFX].'.php');   // ЦЫМЕС
 		}else{
 			return $this->ifError('error code -1 : [method name] method_file_not_exists');
 		}
 	}
-	
+
+
 	private function getEndPoint(){
 		//вернет  путь относительно директории этого файла
 		return str_replace( basename( __FILE__ ) , '' , $_SERVER['SERVER_NAME']. $_SERVER['PHP_SELF'] );///!!!ATENTION
 	}
-	
+
 	protected function idempothyRequest($delete = true){
 		//https://www.w3.org/Protocols/rfc2616/rfc2616-sec9.html
-		if($_GET and !$_POST) { 
-			$_POST = $_GET; 
+		if($_GET and !$_POST) {
+			$_POST = $_GET;
 			if($delete)$_GET = null;
 		}
 		else { return 0; }
 	}
-	
-	
+
+
 
 }
 
@@ -80,8 +81,8 @@ api::getApi()->load();
 /*site.com/path?v=1&method=space&param1=val1 ...
 |--path:
 --->inpex.php
-	<?php include_once "api.php"; 
-		
+	<?php include_once "api.php";
+
 	?>
 */
 
